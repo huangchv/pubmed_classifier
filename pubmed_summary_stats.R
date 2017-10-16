b952aea0f56ca6fe1fdab2115672e1a69b3f6010
huangchv <vince.ch.huang@gmail.com>
2017-10-16 00:14:08 -0400

    Adding more summary plots

--- pubmed_summary_stats.R
+++ pubmed_summary_stats.R
@@ -1,3 +1,4 @@
+library(plyr)
 library(dplyr)
 library(tibble)
 library(reshape2)
@@ -34,17 +35,87 @@ create.scatterplot(
 create.boxplot(
   all.tfidf.full,
   formula = journal ~ citations,
+  width = 6,
+  height = 3, 
+  resolution = 300,
+  filename = generate.filename('journal','citations','png'),
   outliers = FALSE,
   xlimits = c(-20,350),
   add.stripplot = TRUE,
-  xlab.cex = 1.5,
-  ylab.cex = 1.5,
+  xlab.cex = 1.25,
+  ylab.cex = 1.25,
   xlab.label = 'Citations',
   ylab.label = '',
   yaxis.lab = journal.lookup$journals,
-  yaxis.cex = 1.25,
-  xaxis.cex = 1.25,
+  yaxis.cex = 1,
+  xaxis.cex = 1,
   style = 'Nature',
   points.col = 'darkorange',
-  points.alpha = 1
+  points.alpha = 0.75,
+  left.padding = 1,
+  right.padding = 1
   )
+
+##### Months submitted with more data #####
+months.submitted <- aggregate(all.tfidf.full$month_sub, list(all.tfidf.full$journal), unlist(table))
+sapply(months.submitted$x, unlist)
+
+
+months.sub.toplot <- data.frame(matrix(c(unlist(months.submitted$x), rep(0,12)) , ncol=12, byrow = TRUE))
+colnames(months.sub.toplot) <- month.abb[1:12]
+rownames(months.sub.toplot) <- journal.lookup$journals
+
+months.sub.toplot$sum <- apply(months.sub.toplot, 1, sum)
+months.sub.percent.toplot <- months.sub.toplot[,1:12] / months.sub.toplot[,13]
+months.sub.percent.toplot$journal <- rownames(months.sub.percent.toplot)
+months.sub.percent.melt <- melt(months.sub.percent.toplot[1:8,])
+months.sub.percent.melt$variable <- as.numeric(months.sub.percent.melt$variable)
+
+months.sub.percent.melt$journal <- as.factor(months.sub.percent.melt$journal)
+
+line.lty <- rep(1,8)
+line.lty[c(2,5)] <- 2 
+line.lty[8] <- 3 
+
+journal.cols <- c('darkorange', 'darkorange', 'chartreuse4', 'darkorchid4', 'darkorchid4', "dodgerblue", "firebrick3", 'darkorchid4')
+
+create.scatterplot(
+  data = months.sub.percent.melt,
+  formula = value ~ variable,
+  groups = journal,
+  col = journal.cols,
+  type = c('l', 'g'),
+  width = 8,
+  height = 5,
+  filename = generate.filename('papers','month_sub','png'),
+  style = 'Nature',
+  resolution = 300,
+  lwd = 2,
+  lty = line.lty,
+  xlimits = c(0.5, 12.5),
+  xat = 1:12,
+  ylimits = c(-0.02, 0.22),
+  xaxis.lab = month.abb,
+  xlab.label = '',
+  xaxis.cex = 1,
+  yaxis.cex = 1,
+  ylab.label = 'Papers (%)',
+  ylab.cex = 1.15,
+  key = list(
+    text = list(
+      lab = as.vector(journal.lookup$journals[1:8]),
+      cex = 1,
+      col = 'black'
+    ),
+    points = list(
+      pch = 19,
+      col = journal.cols,
+      cex = 1
+    ),
+    x = 0.70,
+    y = 0.95,
+    padding.text = 2
+  ),
+  left.padding = 1,
+  right.padding = 1
+)
